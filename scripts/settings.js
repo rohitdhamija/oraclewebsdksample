bc05bc2665d43017882891bea44f9ef36ae14aef
rohitdhamija <rohit.dhamija@gmail.com>
2019-12-27 11:03:27 +0530

    updatedautocomplete

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,2 +1,2 @@
 const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
-const YOUR_CHANNELID = 'dc45b973-d64a-4cc2-ad5b-2f8263832425';
+const YOUR_CHANNELID = '128c65cb-e967-499e-9a95-473ad9f2edad';
