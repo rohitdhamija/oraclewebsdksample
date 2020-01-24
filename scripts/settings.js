f96acb83ba1e8e166180c247affc2b4cd4eee82e
rohitdhamija <rohit.dhamija@gmail.com>
2020-01-24 21:30:45 +0530

    changedsettings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,4 @@
 //const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
 //const YOUR_CHANNELID = '7afe761d-3c96-4cb8-a55b-3f9a790af1e3';
-const YOUR_URI = 'oda-1bcf25720e924543a27920c68219b6cd-da12.data.digitalassistant.oci.oraclecloud.com';
-const YOUR_CHANNELID = '669504e3-f1ab-4240-a0d2-f3c8899b4dac';
+const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
+const YOUR_CHANNELID = '76696db1-16c7-419c-87f9-ae58a073ed57';
