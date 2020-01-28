acaa54bdc2c3ae83e2ef29e6fc2916a6e798406d
rohitdhamija <rohit.dhamija@gmail.com>
2020-01-28 16:21:11 +0530

    settings

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,4 @@
 //const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
 //const YOUR_CHANNELID = '7afe761d-3c96-4cb8-a55b-3f9a790af1e3';
 const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
-const YOUR_CHANNELID = '76696db1-16c7-419c-87f9-ae58a073ed57';
+const YOUR_CHANNELID = '7afe761d-3c96-4cb8-a55b-3f9a790af1e3';
