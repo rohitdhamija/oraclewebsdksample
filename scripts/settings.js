535184a8bdb14f01a7b507b41aa73dbdceb7b5b4
rohitdhamija <rohit.dhamija@gmail.com>
2020-02-14 20:47:51 +0530

    fileuplaod

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,4 @@
 //const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
 //const YOUR_CHANNELID = '7afe761d-3c96-4cb8-a55b-3f9a790af1e3';
-const YOUR_URI = 'oda-db591e3f7b5f4a9290f3244ef44c516f-s0.data.digitalassistant.oci.oc-test.com';
-const YOUR_CHANNELID = 'c6149a8d-4d3a-4fdd-be05-13f8b42b85f5';
+const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
+const YOUR_CHANNELID = '76696db1-16c7-419c-87f9-ae58a073ed57';
