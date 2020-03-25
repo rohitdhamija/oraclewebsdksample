5ff86b0fb186035f8d1d3e09a0184f025d56572a
rohitdhamija <rohit.dhamija@gmail.com>
2020-03-25 14:15:58 +0530

    nhscovid

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,5 @@
 //const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
 //const YOUR_CHANNELID = '7afe761d-3c96-4cb8-a55b-3f9a790af1e3';
-const YOUR_URI = 'idcs-oda-1e9949f958a242bab9524b1bb4bbff7a-s0.data.digitalassistant.oci.oc-test.com';
-const YOUR_CHANNELID = 'a3054c3b-bf6b-48fc-a177-535ad71f6f6b';
+const YOUR_URI = 'oda-eb4eb80896a6440f9071b927b13bbb16-da2.data.digitalassistant.oci.oraclecloud.com';
+const YOUR_CHANNELID = 'e995d79c-01c9-4ca3-8cae-037ae34fdcc9';
+
