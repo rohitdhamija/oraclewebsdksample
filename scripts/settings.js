6b3d0ce1fdd4e1884afcdaa503cca601a4a61159
rohitdhamija <rohit.dhamija@gmail.com>
2020-03-25 13:45:09 +0530

    nhscovid

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,4 @@
 //const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
 //const YOUR_CHANNELID = '7afe761d-3c96-4cb8-a55b-3f9a790af1e3';
-const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
-const YOUR_CHANNELID = '76696db1-16c7-419c-87f9-ae58a073ed57';
+const YOUR_URI = 'idcs-oda-1e9949f958a242bab9524b1bb4bbff7a-s0.data.digitalassistant.oci.oc-test.com';
+const YOUR_CHANNELID = 'a3054c3b-bf6b-48fc-a177-535ad71f6f6b';
