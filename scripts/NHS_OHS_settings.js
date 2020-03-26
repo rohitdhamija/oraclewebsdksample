60f88cef3baaeaa04580301f5a94e52906201caf
rohitdhamija <rohit.dhamija@gmail.com>
2020-03-26 16:51:25 +0530

    NHS_OHS

---
+++ scripts/NHS_OHS_settings.js
@@ -0,0 +1,5 @@
+//const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
+//const YOUR_CHANNELID = '7afe761d-3c96-4cb8-a55b-3f9a790af1e3';
+const YOUR_URI = 'oda-eb4eb80896a6440f9071b927b13bbb16-da2.data.digitalassistant.oci.oraclecloud.com';
+const YOUR_CHANNELID = '41f05d3b-3f70-4cf2-bbac-563fc1320f05';
+
