39ccb9cade17c85833c61866a2241cbf4e503c25
rohitdhamija <rohit.dhamija@gmail.com>
2020-04-16 14:29:15 +0530

    test

---
+++ scripts/NHS_SysAccess_test_settings.js
@@ -0,0 +1,5 @@
+//const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
+//const YOUR_CHANNELID = '7afe761d-3c96-4cb8-a55b-3f9a790af1e3';
+const YOUR_URI = 'oda-db591e3f7b5f4a9290f3244ef44c516f-s0.data.digitalassistant.oci.oc-test.com';
+const YOUR_CHANNELID = 'bf4d8477-958c-41e8-a4b9-f770812b382b';
+
---
+++ scripts/NHS_Test.js
@@ -0,0 +1,4 @@
+//const YOUR_URI = 'oda-5b8277bc39c14499bed752a6fdd94c30-s0.data.digitalassistant.oci.oc-test.com';
+//const YOUR_CHANNELID = '7afe761d-3c96-4cb8-a55b-3f9a790af1e3';
+const YOUR_URI = 'idcs-oda-1b53c857f8724ac1a72cd710c0f5d5be-s0.data.digitalassistant.oci.oc-test.com';
+const YOUR_CHANNELID = '371e33cf-24da-44b6-87f2-96c02b5bbea6';
